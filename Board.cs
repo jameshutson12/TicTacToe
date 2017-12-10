5f45913d8e5530be053164c250b28a4ec8fc9db6
James Hutson <jameshutson05@gmail.com>
2017-12-10 16:05:41 +0000

    Initial release. Two players can play .vs each other.

--- Board.cs
+++ Board.cs
@@ -1,39 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TicTacToe
 {
     public class Board
     {
         private string[,] positions = new string[3, 3];
+        private List<int> validMoves = new List<int>();
 
         public Board()
         {
+            // Initialise all board positions to " ".
             for (int i = 0; i < 3; i++)
             {
                 // Initialise the board.
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     positions[i, j] = " ";
                 }
             }
+
+            // Initialise out valid moves list.
+            validMoves.Add(0);
+            validMoves.Add(1);
+            validMoves.Add(2);
         }
 
         /// <summary>
-        /// Prints the board.
+        /// Prints the board to the console.
         /// </summary>
         public void PrintBoard()
         {
-            // TODO: Need to add column, row numbers to board.
-
-            // This will be the down.
+            // This represent the columns.
             for (int i = 0; i < 3; i++)
             {
-                // This will be the across.
-                for (int j = 0; j < 2; j++)
+                // This represent the rows.
+                for (int j = 0; j < 3; j++)
                 {
-                    Console.Write($" {positions[i, j]} |");
+                    // Logic around pretty printing the board.
+                    if (i == 0 && j== 0) Console.WriteLine($"  0   1   2");
+                    if (j == 0) Console.Write(i);
+                    if (j == 2) 
+                    {
+                        Console.Write($" {positions[i, j]}");   
+                    }
+                    else
+                    {
+                        Console.Write($" {positions[i, j]} |");   
+                    }
                 }
                 Console.WriteLine();
-                if (i < 2) Console.WriteLine("-----------");
+                if (i < 2) Console.WriteLine("  ----------");
             }
         }
 
@@ -41,21 +58,104 @@ namespace TicTacToe
         /// <summary>
         /// Perform a move on the board.
         /// </summary>
-        /// <param name="move">Move the player has made.</param>
-        /// <param name="turn">True if turn is player 'X', otherwise False for player 'O'</param>
-        public void MakeMove(string move, bool turn)
+        /// <param name="MoveRow">The player's row to insery symbol onto.</param>
+        /// <param name="MoveColumn">The player's column to insery symbol onto.</param>
+        /// <param name="CurrentPlayer">Current player's symbol.</param>
+        public void MakeMove(int MoveRow, int MoveColumn, string CurrentPlayer)
         {
-            // Split move into it's two parts (Column, Row) i.e. '0', '1'.
-            int column = Convert.ToInt32(move.Substring(0, 1));
-            int row = Convert.ToInt32(move.Substring(1, 1));
-
-            positions[column, row] = (turn) ? "X" : "O";
+            positions[MoveColumn, MoveRow] = (CurrentPlayer == "X") ? "X" : "O";
         }
 
 
-        public void CheckWinner()
+        /// <summary>
+        /// Checks if we have a winner.
+        /// </summary>
+        /// <returns>True, if we have a winner, False otherwise.</returns>
+        /// <param name="CurrentPlayer">Current player symbol</param>
+        public bool CheckWinner(ref string CurrentPlayer)
         {
-            // TODO: Go through all permutations to see if we have a winner.
+            bool retVal = false;
+
+            // Loop through all winning combinations to check for a winner.
+            // First loop through rows for a winner.
+            for (int i = 0; i < 3; i++)
+            {
+                if (positions[i, 0] == CurrentPlayer && positions[i, 1] == CurrentPlayer && positions[i, 2] == CurrentPlayer)
+                {
+                    retVal = true;
+                }
+            }
+
+            // Next loop through columns for a winner.
+            for (int i = 0; i < 3; i++)
+            {
+                if (positions[0, i] == CurrentPlayer && positions[1, i] == CurrentPlayer && positions[2, i] == CurrentPlayer)
+                {
+                    retVal = true;
+                }
+            }
+
+            // Finally check diagonals for a winner.
+            if (positions[0, 0] == CurrentPlayer && positions[1, 1] == CurrentPlayer && positions[2, 2] == CurrentPlayer)
+            {
+                retVal = true;
+            }
+
+            if (positions[0, 2] == CurrentPlayer && positions[1, 1] == CurrentPlayer && positions[2, 0] == CurrentPlayer)
+            {
+                retVal = true;
+            }
+
+            // Have we come across a winning combination so far.
+            if (!retVal)
+            {
+                bool isTie = true;
+                // No, let's check for a draw. Loop through game board looking for a empty tile.
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        // Is this an empty tile.
+                        if (positions[i, j] == " ")
+                        {
+                            // Yes, so we have not tied yet.
+                            isTie = false;
+                        }
+                    }
+                }
+                if (isTie)
+                {
+                    CurrentPlayer = null;
+                    retVal = true;
+                }
+            }
+
+            return retVal;
         }
+
+
+        /// <summary>
+        /// Is the player's move valid on the current game board.
+        /// </summary>
+        /// <param name="MoveRow">The row for the move.</param>
+        /// <param name="MoveColumn">The column for the move.</param>
+        /// <returns>True is move is valid, otherwise False.</returns>
+        public bool IsMoveValid(int MoveRow, int MoveColumn)
+        {
+            bool retVal = false;
+
+            // Is the move within the valid moves list.
+            if (validMoves.Contains(MoveRow) && validMoves.Contains(MoveColumn))
+            {
+                // Is the move onto an empty tile.
+                if (positions[MoveColumn, MoveRow] == " ")
+                {
+                    retVal = true;
+                }
+            }
+
+            return retVal;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,16 +4,123 @@ namespace TicTacToe
 {
     class Program
     {
+        static Board board;
+        static bool lastMoveSuccess = true;
+
         static void Main(string[] args)
         {
+            // TODO: Make it possible to play against the computer.
             // First we need to decide if we are playing the computer or human.
             Console.WriteLine("Do you want to play .vs the computer? Y/N");
-            Board board = new Board();
+            board = new Board();
+            string currentPlayer = "O";
+
+            // Loop until we have a satisfactory answer.
+            string response = Console.ReadLine();
+            while (response.ToUpper() != "N")
+            {
+                response = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter Co-Ordindate in Row/Column");
+            // Print the initial game board.
             board.PrintBoard();
 
-            // TOOD: Need to make the main game loop.
+            // Play main game loop until we have a winner or tie.
+            while(!board.CheckWinner(ref currentPlayer))
+            {
+                // Was our last iteration a sucessful move.
+                if (!lastMoveSuccess)
+                {
+                    Console.WriteLine($"Invalid move, try again player '{currentPlayer}'");
+                }
+                else
+                {
+                    currentPlayer = SetNextPlayer(currentPlayer);
+                    Console.WriteLine($"Make your Move... Player '{currentPlayer}'");
+                }
+
+                // Get the move.
+                string move = Console.ReadLine();
+                int moveRow = -1;
+                int moveColumn = -1;
+                parseMove(move, ref moveRow, ref moveColumn);
+
+                // Do we have a valid move entered.
+                if (moveRow != -1 && moveColumn != -1)
+                {
+                    // Yes, let's make the move in the board.
+                    board.MakeMove(moveRow, moveColumn, currentPlayer);
+                    lastMoveSuccess = true;
+                }
+                else
+                {
+                    lastMoveSuccess = false;
+                }
+
+                // Print the board to show the last move.
+                board.PrintBoard();
+            }
+
+            // Stop the program quitting when we have a winner.
+            if (currentPlayer == null)
+            {
+                Console.WriteLine($"We have a TIE");
+            }
+            else
+            {
+                Console.WriteLine($"We have a winner! Player {currentPlayer} WINS!");    
+            }
 
             Console.ReadLine();
         }
+
+
+        /// <summary>
+        /// Switches the current player
+        /// </summary>
+        /// <returns>The next player.</returns>
+        /// <param name="CurrentPlayer">Current player.</param>
+        private static string SetNextPlayer(string CurrentPlayer)
+        {
+            string retVal = "";
+            if (CurrentPlayer == "X")
+            {
+                retVal = "O";
+            }
+            else
+            {
+                retVal = "X";
+            }
+
+            return retVal;
+        }
+
+
+        /// <summary>
+        /// Parses the move.
+        /// </summary>
+        /// <param name="move">The player's move, non-parsed.</param>
+        /// <param name="MoveRow">The player's row move, parsed</param>
+        /// <param name="MoveColumn">Move column.</param>
+        /// <remarks>Ref parameters as they are used in the calling method to determine a valid move has been entered.</remarks>
+        public static void parseMove(string move, ref int MoveRow, ref int MoveColumn)
+        {
+            // Do we have two characters to represent co-ordinates.
+            if (move.Length == 2)
+            {
+                // Split move into it's two parts (Column, Row) i.e. '0', '1'.
+                MoveColumn = Convert.ToInt32(move.Substring(0, 1));
+                MoveRow = Convert.ToInt32(move.Substring(1, 1));
+                // Check whether the move is valid on the current game board.
+                if (!board.IsMoveValid(MoveRow, MoveColumn))
+                {
+                    // No, so set ref values to signify failure.
+                    MoveColumn = -1;
+                    MoveRow = -1;
+                }
+            }
+        }
+
     }
 }
